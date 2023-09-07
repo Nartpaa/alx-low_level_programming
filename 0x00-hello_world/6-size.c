8b0483801502aefa72c89d5d0afcc3ef28bca721
Emmanuel Nartey <knartish@gmail.com>
2023-09-07 12:25:12 -0700

    Territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,5 +14,6 @@ int main(void)
 	printf("Size of float:: %d byte(s)\n", sizeof(float));
 	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+
 	return (0);
 }
